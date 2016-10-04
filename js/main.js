a4964394d455ceb6734e0fd4c52725307ebfc99c
Belchenkov <u608100@gmail.com>
2016-10-04 18:46:32 +0300

    Scrolling link

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
-/**
- * Created by Belchenkov Aleksey on 07.09.2016.
- */
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() > 970) {
+            $('.navbar').addClass('navbar-fixed-top');
+        } else if($(window).scrollTop() < 971) {
+            $('.navbar').removeClass('navbar-fixed-top');
+        }
+
+    });//end window
+}); //end ready()
+
+$(".navbar-dark ul li a[href^='#']").on('click', function (e) {
+    e.preventDefault();
+
+    var hash = this.hash;
+
+    $("html, body").animate({
+        scrollTop: $(hash).offset().top},
+        400,
+        function () {
+            window.location.hash = hash;
+        }
+    ); // end animate()
+}); // end on('click')
+
+$("#section-parallax .col-sm-12 a[href^='#']").on('click', function (e) {
+    e.preventDefault();
+
+    var hash = this.hash;
+
+    $("html, body").animate({
+            scrollTop: $(hash).offset().top},
+        400,
+        function () {
+            window.location.hash = hash;
+        }
+    ); // end animate()
+}); // end on('click')
+
+$(".footer .scroll-to-top-button a[href^='#']").on('click', function (e) {
+    e.preventDefault();
+
+    var hash = this.hash;
+
+    $("html, body").animate({
+            scrollTop: $(hash).offset().top},
+        400,
+        function () {
+            window.location.hash = hash;
+        }
+    ); // end animate()
+}); // end on('click')
