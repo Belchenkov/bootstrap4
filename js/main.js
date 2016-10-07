e0f211d510f42860d285396493f9967ce91540e8
Belchenkov <u608100@gmail.com>
2016-10-07 10:23:29 +0300

    animate blocks

--- js/main.js
+++ js/main.js
@@ -52,3 +52,35 @@ $(".footer .scroll-to-top-button a[href^='#']").on('click', function (e) {
         }
     ); // end animate()
 }); // end on('click')
+
+$('.js-wp-1').waypoint(function (direction) {
+    $('.js-wp-1').addClass('animated fadeInDown');
+    },
+    {
+       offset: '50%'
+    }
+); // end waypoint()
+
+$('.js-wp-2').waypoint(function (direction) {
+    $('.js-wp-2').addClass('animated fadeInLeft');
+    },
+    {
+       offset: '50%'
+    }
+); // end waypoint()
+
+$('.js-wp-3').waypoint(function (direction) {
+    $('.js-wp-3').addClass('animated fadeInRight');
+    },
+    {
+       offset: '50%'
+    }
+); // end waypoint()
+
+$('.js-wp-4').waypoint(function (direction) {
+        $('.js-wp-4').addClass('animated fadeInUp');
+    },
+    {
+        offset: '50%'
+    }
+); // end waypoint()
